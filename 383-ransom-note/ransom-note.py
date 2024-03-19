f91460bb835ba72736e6afef7bb3eb07647cf3ac
Lea Albano <89825014+leaalbano@users.noreply.github.com>
2024-03-19 13:05:59 -0700

    Time: 64 ms (34.52%) | Memory: 16.8 MB (44.29%) - LeetSync

--- 383-ransom-note/ransom-note.py
+++ 383-ransom-note/ransom-note.py
@@ -6,11 +6,9 @@ class Solution:
                 ransom[char] += 1
             else:
                 ransom[char] = 1
-        print(ransom)
         
         for ch in ransomNote:
             if ch in ransom and ransom[ch] > 0:
-                print(ransom)
                 ransom[ch] -= 1
             else:
                 return False
